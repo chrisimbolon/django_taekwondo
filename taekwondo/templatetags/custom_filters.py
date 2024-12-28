654f83a1862579c909d12de243008a38b571be83
Christyan Simbolon <sambungdata@gmail.com>
2024-12-29 01:38:58 +0700

    changing behavior of card with fullname truncating

--- taekwondo/templatetags/custom_filters.py
+++ taekwondo/templatetags/custom_filters.py
@@ -9,8 +9,32 @@ def add_class(field, css_class):
 
 
 @register.filter
-def truncate_chars(value, max_length):
-    """Truncate a string after a certain number of characters."""
-    if len(value) > max_length:
-        return value[:max_length] + "..."
-    return value
+def smart_truncate(value, max_length=20):
+    """
+    Truncate a string at the nearest word boundary without exceeding max_length.
+    Adds an ellipsis ("...") if truncated.
+    """
+    if len(value) <= max_length:
+        return value
+    
+    truncated = value[:max_length].rsplit(' ', 1)[0]
+    return f"{truncated}..."
+
+@register.filter
+def gender_card_class(coach):
+    """Returns card CSS class based on gender."""
+    return "boy-card" if coach.sex == "male" else "girl-card"
+
+@register.filter
+def gender_icon(coach):
+    """Returns font-awesome gender icon class based on gender."""
+    return "fa-mars" if coach.sex == "male" else "fa-venus"
+
+@register.filter
+def profile_picture(coach):
+    """Returns the appropriate profile picture URL."""
+    if coach.photo:
+        return coach.photo.url
+    if coach.sex == "male":
+        return static("images/c-man.png")
+    return static("images/c-woman.png")
