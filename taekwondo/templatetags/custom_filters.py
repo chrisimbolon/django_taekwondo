4ed32bb3041fab552416b77746732a09b56e7516
Christyan Simbolon <sambungdata@gmail.com>
2024-12-28 23:57:03 +0700

    attempting to truncate fullname on card

--- taekwondo/templatetags/custom_filters.py
+++ taekwondo/templatetags/custom_filters.py
@@ -6,3 +6,11 @@ register = template.Library()
 def add_class(field, css_class):
     """Adds a CSS class to form fields."""
     return field.as_widget(attrs={"class": css_class})
+
+
+@register.filter
+def truncate_chars(value, max_length):
+    """Truncate a string after a certain number of characters."""
+    if len(value) > max_length:
+        return value[:max_length] + "..."
+    return value
