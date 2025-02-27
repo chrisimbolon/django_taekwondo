365c55fba22795c0ef663142f8410568810451c0
Christyan Simbolon <sambungdata@gmail.com>
2025-02-27 23:57:20 +0700

    refactoring settings.py to adopt new Database

--- coachathlete/settings.py
+++ coachathlete/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'coachathlete.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-taekwondo-DB',
+        'USER': 'chris-admin',
+        'PASSWORD': 'chris-admin-superuser7',
+        'HOST': 'localhost',  # Or your database server
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
