0d0ced8d36fa838864a7ab65f244f5f97f0537b2
Christyan Simbolon <sambungdata@gmail.com>
2025-04-04 19:14:55 +0700

    troubleshooting urls-attempt three

--- coachathlete/settings.py
+++ coachathlete/settings.py
@@ -141,9 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-STATIC_URL = FORCE_SCRIPT_NAME + "/static/"
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR,'static'),
 )
@@ -151,8 +150,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Setting media files
-# MEDIA_URL = '/media/'
-MEDIA_URL = FORCE_SCRIPT_NAME + "/media/"
+MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #crispy form
